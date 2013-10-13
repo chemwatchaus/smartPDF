d893e913be0cd9cc864730efb6cb640c83e01756
paynd <a.kupreychik@gmail.com>
2013-10-13 22:40:36 +0300

    initial commit of SmartPDF

---
+++ src/net/chemwatch/smartpdfviewer/SmartDocument.java
@@ -0,0 +1,13 @@
+package net.chemwatch.smartpdfviewer;
+
+import java.io.Serializable;
+
+public class SmartDocument implements Serializable {
+	private static final long serialVersionUID = 4663825548483567441L;
+	public String materialName;
+	public String materialId;
+	public String link;
+	public String type;
+	public String language;
+	public String country;
+}
